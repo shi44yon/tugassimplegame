324ac0654d75c9c9690fd76c04145bcc2997c640
Bayu Prajanata <33504580+shi44yon@users.noreply.github.com>
2017-12-13 11:47:52 +0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,25 +1,29 @@
 #include <stdio.h>
-#include<conio.h>
+#include <conio.h>
+//#include <iostream>
+
+//using namespace std;
+
 main() {
     int i=0;
     char kata[9]={'k','e','l','e','l','a','w','a','r'};
     char bintang[9]={'*','*','*','*','*','*','*','*','*'};
     char tebak;
-    int salah=0, menang=0;
+    int kesempatan=3;
     int if_same=0;
     int jumlah_bintang=0;
     printf("Game Tebak Kata\n");
     printf("#Pertanyaan : ");
-    printf("\tHewan apakah yang memiliki kemampuan ekolokasi ?\n");
-    printf("_____________________________\n");
-    while(salah<=3) {
-        printf ("Jawaban :\n");
+    printf("Hewan apakah yang memiliki kemampuan ekolokasi ?\n");
+    printf(" ____________________________________________________________\n");
+    while(kesempatan>=0) {
+        printf ("\nJawaban :\n");
         for(i=0;i<9;i++) {
-            printf("%c ",bintang[i]);
+            printf("%c",bintang[i]);
         }
-		printf("\tJawaban salah %i kali",salah);
+		printf("\tKesempatan menjawab %i kali",kesempatan);
 		printf("\n");
-		printf("\tMasukkan Tebakan Anda : ");
+		printf("\tMasukkan Tebakan Anda :");
 		jumlah_bintang=0;
 		for(i=0;i<9;i++) {
 			if(bintang[i]=='*') {
@@ -35,15 +39,16 @@ main() {
                 if_same++;
             }
         }
-        if(if_same==0) {
-            salah++;
+        if(!if_same) {
+            kesempatan--;
         }
-        if (salah==3) {
-            printf("\nSalah tiga kali silahkan coba lagi ");
+        if (kesempatan==0) {
+            printf("\nKesempatan habis, silahkan coba lagi!");
             break;
         }
         if(jumlah_bintang==0) {
             printf("\nSelamat jawaban anda benar!");
+            printf("\nJawaban :\nkelelawar");
             break;
         }
         if_same=0;
