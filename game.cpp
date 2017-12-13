73960c1aa947ca0ef2ee8dcaafd97b511fcee380
Bayu Prajanata <33504580+shi44yon@users.noreply.github.com>
2017-12-13 11:28:49 +0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -5,50 +5,47 @@ main() {
     char kata[9]={'k','e','l','e','l','a','w','a','r'};
     char bintang[9]={'*','*','*','*','*','*','*','*','*'};
     char tebak;
-    int salah=0,kesempatan=3,menang=0;
-    printf("\n\tGame Tebak Kata\n");
-    printf("\t\n\t#Pertanyaan : \n");
-    printf("\t\tHewan apakah yang memiliki kemampuan ekolokasi ?\n");
-    printf("_____________________________\n\n");
+    int salah=0, menang=0;
+    int if_same=0;
+    int jumlah_bintang=0;
+    printf("Game Tebak Kata\n");
+    printf("#Pertanyaan : ");
+    printf("\tHewan apakah yang memiliki kemampuan ekolokasi ?\n");
+    printf("_____________________________\n");
     while(salah<=3) {
-        if(salah==3) {
-            kesempatan=0;
-            break;
-        }
-        int jumlah_bintang=0;
-        printf ("==> Apakah Jawabannya ? <==\n\n");
+        printf ("Jawaban :\n");
         for(i=0;i<9;i++) {
             printf("%c ",bintang[i]);
         }
-		printf("\tKesempatan Menebak Masih %i kali",kesempatan);
-		printf("\n\n");
-		int if_same=0;
+		printf("\tJawaban salah %i kali",salah);
+		printf("\n");
 		printf("\tMasukkan Tebakan Anda : ");
+		jumlah_bintang=0;
 		for(i=0;i<9;i++) {
 			if(bintang[i]=='*') {
 				jumlah_bintang++;
 			}
 		}
-        if(jumlah_bintang==0) {
-            printf("\n\n Selamat jawaban anda benar!\n");
-            printf("\n________Congratulation________");
-            break;
-        }
         scanf("%c",&tebak);
         fflush(stdin);
         for(i=0;i<9;i++) {
             if(kata[i]==tebak) {
                 bintang[i]=tebak;
-                if_same+=1;
+                jumlah_bintang--;
+                if_same++;
             }
         }
         if(if_same==0) {
             salah++;
-            kesempatan--;
         }
         if (salah==3) {
-            printf("\nMaaf anda harus mencoba lagi !");
+            printf("\nSalah tiga kali silahkan coba lagi ");
             break;
-       }
+        }
+        if(jumlah_bintang==0) {
+            printf("\nSelamat jawaban anda benar!");
+            break;
+        }
+        if_same=0;
     }
 }
